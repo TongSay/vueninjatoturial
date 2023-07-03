06b9eca59b23899fe8b152953e10ebefbb6a1f4e
TongSay <tongsayofficail@gmail.com>
2023-07-03 15:39:47 +0700

    update

---
+++ src/components/Block.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="block">
+        click me
+    </div>
+</template>
+
+<script>
+    export default {
+        props:['delay']
+    }
+</script>
+
+<style>
+
+.block {
+    width: 400px;
+    height: 50px;
+    border-radius: 20px;
+    color: white;
+    background-color: #6af953;
+    text-align: center;
+    justify-content: center;
+    padding: 100px auto;
+    margin: 40px auto;
+}
+
+</style>
